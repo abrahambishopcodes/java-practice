bf93165ea7d9a4471ac13b7d62dffe27231943b2
Abraham Bishop <156090561+abrahambishopcodes@users.noreply.github.com>
2025-09-17 14:08:49 +0100

    multiplication table with java

---
+++ multiplication.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+     int number = 6;
+        
+    for (int x = 1; x <= 12; x++) {
+        System.out.println(number + " x " + x + " = " + number*x);
+    }
+     
+    }
+}
